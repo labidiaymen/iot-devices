de18c855ef31fcc706be2f166f366930e15d9c81
Aymen <labidi@aymen.co>
2021-02-02 17:33:28 +0100

    add const

--- MAIN.ino
+++ MAIN.ino
@@ -3,13 +3,17 @@
  * Vaulab iot sensor project 
  * 2021
  */
-// #define POWER_CONSUMPTION_SENSOR
-
+//#define POWER_CONSUMPTION_SENSOR
+#define DEBUG
 void setup(){
     Serial.begin(115200);
     Serial.printf("\t Main -- \n");
     setupWIFI();
     MQTTSetup();
+  #if defined(POWER_CONSUMPTION_SENSOR)
+    powerConsumptionSetup();
+  #endif
+    
 }
 
 
--- PowerConsumption.ino
+++ PowerConsumption.ino
@@ -5,46 +5,63 @@
  */
 #include "EmonLib.h"
 
+// CONSTANTS
+#define CURRENT_VOLTAGE 220
+#define MESUREMENT_INTEVAL 1000
+#define IRMS_VALUE 1480
+#define START_UP_DELAY 10000
+#define TOTAL_MESUREMENT_TO_SEND 30
+#define CT_SENSOR_PIN 34
+#define CT_SENSOR_CALIBRATION 28
+// END CONSTANTS
+
 EnergyMonitor emon1;
 unsigned long lastMeasurement = 0;
 unsigned long timeFinishedSetup = 0;
-short measurements[30];
+short measurements[TOTAL_MESUREMENT_TO_SEND];
 short measureIndex = 0;
 const char *POWER_CONSUMPTION_TOPIC = "esp32/power/watts";
 
-void PowerConsumptionLoop(){
-  unsigned long currentMillis = millis();
 
-                                                                        // If it's been longer then 1000ms since we took a measurement, take one now!
-  if(currentMillis - lastMeasurement > 1000){
-    double amps = emon1.calcIrms(1480);                                 // Calculate Irms only
-    double watt = amps * 220;
+void powerConsumptionSetup(){
+  emon1.current(CT_SENSOR_PIN, CT_SENSOR_CALIBRATION); 
+}
+
+void PowerConsumptionLoop(){
+  
+  unsigned long currentMillis = millis();
+                                                                       // If it's been longer then 1000ms since we took a measurement, take one now!
+  if(currentMillis - lastMeasurement > MESUREMENT_INTEVAL){
+    double amps = emon1.calcIrms(IRMS_VALUE);                           // Calculate Irms only
+    double watt = amps * CURRENT_VOLTAGE;
 
     lastMeasurement = millis();
-
                                                                         // Readings are unstable the first 5 seconds when the device powers on
                                                                         // so ignore them until they stabilise.
-    if(millis() - timeFinishedSetup < 10000){
+    if(millis() - timeFinishedSetup < START_UP_DELAY){
 
     }else{
-      Serial.println(watt);
+      #if defined(DEBUG)
+        Serial.println(watt);
+      #endif
+  
       measurements[measureIndex] = watt;
       measureIndex++;
     }
   }
 
                                                                          // When we have 30 measurements, send them to MQTT!
-  if (measureIndex == 30) {
+  if (measureIndex == TOTAL_MESUREMENT_TO_SEND) {
   
                                                                          // Construct the JSON to send to AWS
     String msg = "{\"readings\": [";
 
-    for (short i = 0; i <= 28; i++){
+    for (short i = 0; i <= TOTAL_MESUREMENT_TO_SEND-2; i++){
       msg += measurements[i];
       msg += ",";
     }
 
-    msg += measurements[29];
+    msg += measurements[TOTAL_MESUREMENT_TO_SEND-1];
     msg += "]}";
     
     MQTT_Publish(POWER_CONSUMPTION_TOPIC,String(msg).c_str());
