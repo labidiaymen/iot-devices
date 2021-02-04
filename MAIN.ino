757eca72d283e80ecadad72df200c6bf30f1e2ee
Aymen <labidi@aymen.co>
2021-02-04 09:55:35 +0100

    DHT sensor

--- MAIN.ino
+++ MAIN.ino
@@ -3,13 +3,29 @@
  * Vaulab iot sensor project 
  * 2021
  */
-#define POWER_CONSUMPTION_SENSOR
+#include "DHT.h"
+
+//#define POWER_CONSUMPTION_SENSOR
 #define DEBUG
+#define DHT11_PIN 4
+#define DHTTYPE DHT11
+
+uint8_t DHTPin = D4; 
+
+DHT dht(DHTPin, DHTTYPE);                
+
+float Temperature;
+float Humidity;
+ 
+
 void setup(){
+   pinMode(DHTPin, INPUT);
+    dht.begin();              
+
     Serial.begin(115200);
     Serial.printf("\t Main -- \n");
-    setupWIFI();
-    MQTTSetup();
+    //setupWIFI();
+    //MQTTSetup();
   #if defined(POWER_CONSUMPTION_SENSOR)
     powerConsumptionSetup();
   #endif
@@ -18,9 +34,19 @@ void setup(){
 
 
 void loop(){
-  wifiLoop();
-  MQTTLoop();
-  float h = analogRead(0);
+  //wifiLoop();
+  //MQTTLoop();
+  Temperature = dht.readTemperature(); // Gets the values of the temperature
+  Humidity = dht.readHumidity(); // Gets the values of the humidity 
+  Serial.print("Temperature = ");
+  Serial.println(Temperature);
+  Serial.print("Humidity = ");
+  Serial.println(Humidity);
+  float h = analogRead(4);
+  delay(1000);
+  
+ // float h = analogRead(0);
+  Serial.println(h);
 if (!isnan(h))
 {
 //Serial.println(h/1023*100);
